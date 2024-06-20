2e90614809f3ba22b8711691c4f72121321e10c9
TruongDN <45661042+NTQTruongDN@users.noreply.github.com>
2024-06-20 15:50:56 +0700

    Add: implement cache-aside

--- caching-strategies/cache-aside/routes/web.php
+++ caching-strategies/cache-aside/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExampleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ExampleController::class, 'index']);
